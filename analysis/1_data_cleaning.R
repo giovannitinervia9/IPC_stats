05c48fdcf95183a31924ddbcb8a7ab69554bc733
Giovanni Tinervia <giovannitinervia9@gmail.com>
2024-12-12 10:27:31 +0100

    reorganized scripts and data cleaning

---
+++ analysis/1_data_cleaning.R
@@ -0,0 +1,23 @@
+rm(list = ls())
+
+# caricamento librerie
+if(!require(tidyverse)) install.packages("tidyverse")
+
+ged241 <- readRDS("ged241.rds")
+
+data <- ged241 |> 
+  filter(conflict_name == "Israel: Palestine" & country == "Israel") |> 
+  select(id, year, side_b, adm_1, latitude, longitude, event_clarity, date_prec,
+         date_start, date_end, deaths_a, deaths_b, deaths_civilians,
+         deaths_unknown, best)
+
+data$event_duration <- as.numeric((data$date_end - data$date_start)/(3600*24))
+
+date_start <- data$date_start
+date_start <- as.Date(date_start, format = "%Y-%m-%d")
+days_since_start <- as.numeric(date_start - min(date_start))
+data$days_since_start <- days_since_start
+
+data$intertimes <- c(NA, diff(data$days_since_start))
+
+write.csv(data, "IS_PAL.csv")
---
+++ analysis/2_place_of_worship.R
@@ -0,0 +1,72 @@
+rm(list = ls())
+library(osmdata)
+library(sf)
+library(tidyverse)
+
+# Definisci l'area geografica (esempio: Israele)
+area2 <- "Israel"
+
+# Query per luoghi di culto
+places_of_worship_i <- opq(bbox = area2) |>
+  add_osm_feature(key = "amenity", value = "place_of_worship") |>
+  osmdata_sf()
+
+# Estrai i dati dei punti (nodi)
+points2 <- places_of_worship_i$osm_points |>
+  select(name, geometry) |>
+  mutate(Latitude = st_coordinates(geometry)[,2],
+         Longitude = st_coordinates(geometry)[,1]) |>
+  st_drop_geometry() # Rimuove la colonna geometrica, se non necessaria
+
+# Rimuovi i punti senza nome
+filtered_places2 <- points2 |>
+  filter(!is.na(name)) |>
+  select(Name = name, Latitude, Longitude)
+
+# Salvataggio dati in un csv
+rownames(filtered_places2) <- NULL
+filtered_places2$id <- 1:nrow(filtered_places2)
+write.csv(filtered_places2, "places_of_worship_israel.csv", row.names = FALSE)
+
+#### filtraggio e merge con il dataset
+
+w_isr <- read.csv("places_of_worship_israel.csv")
+data <- read.csv("IS_PAL.csv")
+xy_j <- as.matrix(w_isr[, c("Latitude", "Longitude")])
+
+
+library(foreach)
+library(doParallel)
+
+# Set up the parallel backend
+num_cores <- round(parallel::detectCores()/2)  
+cl <- makeCluster(num_cores)
+registerDoParallel(cl)
+
+# Parallel computation with foreach
+min_distance_from_i <- foreach(i = 1:nrow(data), .combine = rbind, .packages = "base") %dopar% {
+  # Extract coordinates for the i-th row of 'data'
+  xy_i <- as.numeric(data[i, c("latitude", "longitude")])
+  
+  distance_from_i <- apply(sweep(xy_j, 2, xy_i, "-"), 1, function(x) sum(sqrt(x^2)))
+  
+  # Return the index of the minimum distance
+  c(which.min(distance_from_i), min(distance_from_i))
+}
+
+# Stop the parallel backend
+stopCluster(cl)
+
+nrow(min_distance_from_i)
+
+rownames(min_distance_from_i) <- NULL
+min_distance_from_i <- as.data.frame(min_distance_from_i)
+head(min_distance_from_i)
+colnames(min_distance_from_i) <- c("nearest_pow", "dist_nearest_pow")
+write.csv(min_distance_from_i, "nearest_pow.csv")
+
+data <- data.frame(data, min_distance_from_i)
+write.csv(data, "IS_PAL.csv")
+
+   
+
